289c6608dde70a4d2c44e530de3abcbd2344dfde
jfrianop <jfrianop@gmail.com>
2019-10-13 23:54:31 -0500

    Added Rules on Server

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyA34_kNNkJ6kTmtVvUdG-3dLJcHWMkirMc",
+	authDomain: "to-do-c3de2.firebaseapp.com",
+	databaseURL: "https://to-do-c3de2.firebaseio.com",
+	projectId: "to-do-c3de2",
+	storageBucket: "",
+	messagingSenderId: "451671717472",
+	appId: "1:451671717472:web:b5243fed2e173935a17505",
+	measurementId: "G-QKNHCZ76SX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+var currentUser;
+
 class Task {
 	constructor(toDo) {
 		this.id = toDo.id
-		this.title = toDo.title
-		this.done = toDo.done
+		this.title = toDo.data().name
+		this.done = toDo.data().done
 	}
 
 	addToList() {
@@ -25,18 +43,17 @@ class Task {
 
 //GET todo from server
 const loadToDo = function () {
-	const toDoList = fetch('http://makeitreal-todo.herokuapp.com/todo_items').then(function (response) {
-		return response.json()
-	})
-	return toDoList
+	return db.collection("tasks").where("user", "==", currentUser).get();
 }
 
 //Declares Task objects from the todo loaded from server
 const loadTask = function (toDoList) {
 	let taskList = []
+	console.log(toDoList);
 	toDoList.forEach((toDo) => {
 		taskList.push(new Task(toDo))
 	})
+
 	displayTasks(taskList)
 }
 
@@ -55,47 +72,60 @@ const initialList = function () {
 
 //POST task to server
 const postTask = function (taskTitle) {
-	var url = 'http://makeitreal-todo.herokuapp.com/todo_items'
-	var data = { title: taskTitle }
+	var data = { name: taskTitle, user: currentUser, done: false }
 
-	fetch(url, {
-		method: 'POST', // or 'PUT'
-		body: JSON.stringify(data), // data can be `string` or {object}!
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-		.then((res) => res.json())
-		.catch((error) => console.error('Error:', error))
-		.then(() => initialList())
+	db.collection("tasks").add(data)
+		.then(function (docRef) {
+			console.log("Task added with id: ", docRef.id);
+			initialList();
+		})
+		.catch(function (error) {
+			console.error("Error adding Task: ", error);
+		});
 }
 
 //PATCH task done status to server
 const patchTask = function (taskDone, taskId) {
-	var url = 'http://makeitreal-todo.herokuapp.com/todo_items/' + taskId
-	var data = { done: taskDone }
 
-	fetch(url, {
-		method: 'PATCH', // or 'PUT'
-		body: JSON.stringify(data), // data can be `string` or {object}!
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	}).then(() => initialList())
+	db.collection("tasks").doc(taskId).set({
+		done: taskDone
+	}, { merge: true }).then(() => initialList());
+
 }
 
 //DELETE task from server
 const deleteTask = function (taskId) {
-	var url = 'http://makeitreal-todo.herokuapp.com/todo_items/' + taskId
-
-	fetch(url, {
-		method: 'DELETE', // or 'PUT'
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	}).then(() => initialList())
+	db.collection("tasks").doc(taskId).delete().then(() => initialList());
 }
 
+//Authentication
+$("form").on("submit", function (e) {
+	e.preventDefault();
+
+	var email = $("#exampleInputEmail1").val();
+	var password = $("#exampleInputPassword1").val();
+
+	firebase.auth().signInWithEmailAndPassword(email, password)
+		.catch(function (error) {
+			console.log("Error code: ", error.code);
+			console.log("Error message:", error.message);
+			alert("Nombre de usuario o contraseña no válido.");
+		});
+});
+
+firebase.auth().onAuthStateChanged(function (user) {
+	if (user) {
+		db.collection("users").doc(user.uid).get().then((e) => {
+			currentUser = e.id;
+			$(".login").hide();
+			$(".wrapper").show();
+			initialList();
+		})
+	} else {
+		$(".wrapper").hide();
+		alert("Nombre de usuario o contraseña no válido.");
+	}
+});
 
 //Jquery listeners
 $(document).ready(initialList)
